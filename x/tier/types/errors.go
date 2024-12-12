c43506631f53a5b00f23b5b673d39480732091e8
Ivan <ivanverch@gmail.com>
2024-12-13 06:51:08 +0800

    Add ErrInvalidAmount

--- x/tier/types/errors.go
+++ x/tier/types/errors.go
@@ -13,4 +13,5 @@ var (
 	ErrInvalidRequest = sdkerrors.Register(ModuleName, 1103, "invalid request")
 	ErrInvalidAddress = sdkerrors.Register(ModuleName, 1104, "invalid address")
 	ErrInvalidDenom   = sdkerrors.Register(ModuleName, 1105, "invalid denom")
+	ErrInvalidAmount  = sdkerrors.Register(ModuleName, 1106, "invalid amount")
 )
