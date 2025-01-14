2a8e4a38b588d4b293f54dc3025ab5a3a3ff3cd3
Ivan <ivanverch@gmail.com>
2025-01-14 15:33:12 +0800

    Add Querier to x/acp module, refactor Params query and TestParamsQuery

--- x/acp/keeper/query_params.go
+++ x/acp/keeper/query_params.go
@@ -3,18 +3,16 @@ package keeper
 import (
 	"context"
 
-	sdk "github.com/cosmos/cosmos-sdk/types"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 
 	"github.com/sourcenetwork/sourcehub/x/acp/types"
 )
 
-func (k Keeper) Params(goCtx context.Context, req *types.QueryParamsRequest) (*types.QueryParamsResponse, error) {
+func (q Querier) Params(ctx context.Context, req *types.QueryParamsRequest) (*types.QueryParamsResponse, error) {
 	if req == nil {
 		return nil, status.Error(codes.InvalidArgument, "invalid request")
 	}
-	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	return &types.QueryParamsResponse{Params: k.GetParams(ctx)}, nil
+	return &types.QueryParamsResponse{Params: q.GetParams(ctx)}, nil
 }
