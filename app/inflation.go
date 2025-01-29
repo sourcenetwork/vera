7b5769e2abba2754f02681a26d0003222a232d25
Ivan <ivanverch@gmail.com>
2025-01-29 21:12:48 +0800

    Add custom inflation function

---
+++ app/inflation.go
@@ -0,0 +1,35 @@
+package app
+
+import (
+	"context"
+
+	"cosmossdk.io/math"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	bankkeeper "github.com/cosmos/cosmos-sdk/x/bank/keeper"
+	minttypes "github.com/cosmos/cosmos-sdk/x/mint/types"
+	stakingkeeper "github.com/cosmos/cosmos-sdk/x/staking/keeper"
+	appparams "github.com/sourcenetwork/sourcehub/app/params"
+	tierkeeper "github.com/sourcenetwork/sourcehub/x/tier/keeper"
+)
+
+func ProvideInflationCalculationFn(
+	tierKeeper tierkeeper.Keeper,
+	bankKeeper bankkeeper.Keeper,
+	stakingKeeper *stakingkeeper.Keeper,
+) minttypes.InflationCalculationFn {
+	// Adjust bonded ratio based on the x/tier module developer stake so that it does not affect rewards
+	return func(ctx context.Context, minter minttypes.Minter, params minttypes.Params, bondedRatio math.LegacyDec) math.LegacyDec {
+		devStake := tierKeeper.GetDeveloperStake(sdk.UnwrapSDKContext(ctx))
+		totalSupply := bankKeeper.GetSupply(ctx, appparams.DefaultBondDenom).Amount
+
+		totalBonded, err := stakingKeeper.TotalBondedTokens(ctx)
+		if err != nil {
+			totalBonded = math.ZeroInt()
+		}
+
+		adjustedBonded := totalBonded.Sub(devStake)
+		adjustedBondedRatio := adjustedBonded.ToLegacyDec().QuoInt(totalSupply)
+
+		return minter.NextInflationRate(params, adjustedBondedRatio)
+	}
+}
