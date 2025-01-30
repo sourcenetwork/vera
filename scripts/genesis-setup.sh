619459a0d2dce2b8b853f553f001c031f526f4e0
Ivan <ivanverch@gmail.com>
2025-01-31 02:34:37 +0800

    Update configs and genesis script

--- scripts/genesis-setup.sh
+++ scripts/genesis-setup.sh
@@ -10,7 +10,7 @@ $BIN init $NODE_NAME --chain-id $CHAIN_ID --default-denom="uopen"
 
 $BIN keys add $VALIDATOR --keyring-backend test
 VALIDATOR_ADDR=$($BIN keys show $VALIDATOR -a --keyring-backend test)
-$BIN genesis add-genesis-account $VALIDATOR_ADDR 100000000000open
-$BIN genesis gentx $VALIDATOR 100000000open --chain-id $CHAIN_ID --keyring-backend test
+$BIN genesis add-genesis-account $VALIDATOR_ADDR 1000000000000000uopen # 1b open
+$BIN genesis gentx $VALIDATOR 100000000000000uopen --chain-id $CHAIN_ID --keyring-backend test # 100m open
 
 $BIN genesis collect-gentxs
