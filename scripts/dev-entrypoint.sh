6a3ff93caa9b932a1d50703656445872e6a2a6f4
Ivan <ivanverch@gmail.com>
2025-02-04 14:16:03 +0800

    Update dev-entrypoint.sh

--- scripts/dev-entrypoint.sh
+++ scripts/dev-entrypoint.sh
@@ -2,7 +2,6 @@
 
 if [ ! -e "~/INITIALIZED" ]; then
     scripts/genesis-setup.sh
-    sed -i 's/^timeout_commit = .*/timeout_commit = "1s"/' ~/.sourcehub/config/config.toml
     touch "~/INITIALIZED"
 fi
 
