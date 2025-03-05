e7b3e7f8973055d56cadad093c5854ec5a139a50
Ivan <ivanverch@gmail.com>
2025-03-05 20:49:25 +0800

    Add CreatePriorityLane

--- app/lanes.go
+++ app/lanes.go
@@ -10,13 +10,13 @@ import (
 )
 
 // CreateLanes creates and returns two lanes: priorityLane and defaultLane.
-// Priority lane is a custom lane that matches all transactions related to the x/acp module.
+// Priority lane uses PriorityMatchHandler to match all transactions related to the x/acp and x/tier modules.
 // Default lane is the default BaseLane implementation.
 func CreateLanes(app *App) (priorityLane *base.BaseLane, defaultLane *base.BaseLane) {
 	// signerAdapter is used to extract the expected signers from a transaction
 	signerAdapter := signerextraction.NewDefaultAdapter()
 
-	// Create a priority lane configuration that consumes 40% of the block space
+	// Create a priority lane configuration that occupies 40% of the block space
 	priorityLaneConfig := base.LaneConfig{
 		Logger:          app.Logger(),
 		TxEncoder:       app.txConfig.TxEncoder(),
@@ -26,7 +26,7 @@ func CreateLanes(app *App) (priorityLane *base.BaseLane, defaultLane *base.BaseL
 		MaxTxs:          0,
 	}
 
-	// Create a default lane configuration that consumes 60% of the block space
+	// Create a default lane configuration that occupies 60% of the block space
 	defaultLaneConfig := base.LaneConfig{
 		Logger:          app.Logger(),
 		TxEncoder:       app.txConfig.TxEncoder(),
@@ -36,13 +36,13 @@ func CreateLanes(app *App) (priorityLane *base.BaseLane, defaultLane *base.BaseL
 		MaxTxs:          0,
 	}
 
-	// Create the TxPriority for the priority lane
+	// Create TxPriority for the priority lane
 	priorityLaneTxPriority := lanes.TxPriority()
 
-	// Create the match handler for the priority lane
+	// Create match handler for the priority lane
 	priorityLaneMatchHandler := lanes.PriorityMatchHandler()
 
-	// Create the match handlers for the default lane
+	// Create match handler for the default lane
 	defaultLaneMatchHandler := base.DefaultMatchHandler()
 
 	// Create priority lane
@@ -59,4 +59,35 @@ func CreateLanes(app *App) (priorityLane *base.BaseLane, defaultLane *base.BaseL
 	)
 
 	return priorityLane, defaultLane
+}
+
+// CreatePriorityLane creates a lane that matches all txs and occupies 100% of the block space.
+func CreatePriorityLane(app *App) (priorityLane *base.BaseLane) {
+	// signerAdapter is used to extract the expected signers from a transaction
+	signerAdapter := signerextraction.NewDefaultAdapter()
+
+	// Create a priority lane configuration that occupies 100% of the block space
+	priorityLaneConfig := base.LaneConfig{
+		Logger:          app.Logger(),
+		TxEncoder:       app.txConfig.TxEncoder(),
+		TxDecoder:       app.txConfig.TxDecoder(),
+		MaxBlockSpace:   math.LegacyMustNewDecFromStr("1.0"),
+		SignerExtractor: signerAdapter,
+		MaxTxs:          0,
+	}
+
+	// Create TxPriority for the priority lane
+	priorityLaneTxPriority := lanes.TxPriority()
+
+	// Use default match handler to match all transactions
+	defaultLaneMatchHandler := base.DefaultMatchHandler()
+
+	// Create priority lane
+	priorityLane = lanes.NewPriorityLane(
+		priorityLaneConfig,
+		priorityLaneTxPriority,
+		defaultLaneMatchHandler,
+	)
+
+	return priorityLane
 }
