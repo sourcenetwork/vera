b6afbcebb0694b360eff43e18df9fad533a2c13d
Ivan <ivanverch@gmail.com>
2025-03-11 05:37:05 +0800

    Add accountKeeper and acpKeeper to bulletin keeper

--- x/bulletin/keeper/keeper.go
+++ x/bulletin/keeper/keeper.go
@@ -8,6 +8,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
+	acpkeeper "github.com/sourcenetwork/sourcehub/x/acp/keeper"
 	"github.com/sourcenetwork/sourcehub/x/bulletin/types"
 )
 
@@ -20,6 +21,9 @@ type (
 		// the address capable of executing a MsgUpdateParams message. Typically, this
 		// should be the x/gov module account.
 		authority string
+
+		accountKeeper types.AccountKeeper
+		acpKeeper     acpkeeper.Keeper
 	}
 )
 
@@ -28,23 +32,36 @@ func NewKeeper(
 	storeService store.KVStoreService,
 	logger log.Logger,
 	authority string,
-
+	accountKeeper types.AccountKeeper,
+	acpKeeper acpkeeper.Keeper,
 ) Keeper {
 	if _, err := sdk.AccAddressFromBech32(authority); err != nil {
 		panic(fmt.Sprintf("invalid authority address: %s", authority))
 	}
 
 	return Keeper{
-		cdc:          cdc,
-		storeService: storeService,
-		authority:    authority,
-		logger:       logger,
+		cdc:           cdc,
+		storeService:  storeService,
+		logger:        logger,
+		authority:     authority,
+		accountKeeper: accountKeeper,
+		acpKeeper:     acpKeeper,
 	}
 }
 
 // GetAuthority returns the module's authority.
 func (k Keeper) GetAuthority() string {
 	return k.authority
+}
+
+// GetAcpKeeper returns the module's AcpKeeper.
+func (k Keeper) GetAcpKeeper() *acpkeeper.Keeper {
+	return &k.acpKeeper
+}
+
+// GetAuthKeeper returns the module's AccountKeeper.
+func (k Keeper) GetAccountKeeper() types.AccountKeeper {
+	return k.accountKeeper
 }
 
 // Logger returns a module-specific logger.
