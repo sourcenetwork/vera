f5f50e41c3288316c9df505119c0044391df9b9e
Ivan <ivanverch@gmail.com>
2025-03-17 18:22:21 +0800

    Define all keeper messages and refactor into msg_server

--- x/bulletin/keeper/msg_server_test.go
+++ x/bulletin/keeper/msg_server_test.go
@@ -1,24 +1,71 @@
 package keeper_test
 
 import (
-	"context"
 	"testing"
 
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/stretchr/testify/require"
 
 	keepertest "github.com/sourcenetwork/sourcehub/testutil/keeper"
-	"github.com/sourcenetwork/sourcehub/x/bulletin/keeper"
 	"github.com/sourcenetwork/sourcehub/x/bulletin/types"
 )
 
-func setupMsgServer(t testing.TB) (keeper.Keeper, types.MsgServer, context.Context) {
-	k, ctx := keepertest.BulletinKeeper(t)
-	return k, keeper.NewMsgServerImpl(k), ctx
-}
-
 func TestMsgServer(t *testing.T) {
-	k, ms, ctx := setupMsgServer(t)
-	require.NotNil(t, ms)
+	k, ctx := keepertest.BulletinKeeper(t)
 	require.NotNil(t, ctx)
 	require.NotEmpty(t, k)
+}
+
+func TestMsgUpdateParams(t *testing.T) {
+	k, ctx := keepertest.BulletinKeeper(t)
+	params := types.DefaultParams()
+	require.NoError(t, k.SetParams(ctx, params))
+	wctx := sdk.UnwrapSDKContext(ctx)
+
+	// default params
+	testCases := []struct {
+		name      string
+		input     *types.MsgUpdateParams
+		expErr    bool
+		expErrMsg string
+	}{
+		{
+			name: "invalid authority",
+			input: &types.MsgUpdateParams{
+				Authority: "invalid",
+				Params:    params,
+			},
+			expErr:    true,
+			expErrMsg: "invalid authority",
+		},
+		{
+			name: "send enabled param",
+			input: &types.MsgUpdateParams{
+				Authority: k.GetAuthority(),
+				Params:    types.Params{},
+			},
+			expErr: false,
+		},
+		{
+			name: "all good",
+			input: &types.MsgUpdateParams{
+				Authority: k.GetAuthority(),
+				Params:    params,
+			},
+			expErr: false,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			_, err := k.UpdateParams(wctx, tc.input)
+
+			if tc.expErr {
+				require.Error(t, err)
+				require.Contains(t, err.Error(), tc.expErrMsg)
+			} else {
+				require.NoError(t, err)
+			}
+		})
+	}
 }
