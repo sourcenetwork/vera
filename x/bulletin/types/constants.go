c0dcc3965fe8feca726617090c7ba1a788fbc44e
Ivan <ivanverch@gmail.com>
2025-03-17 17:49:55 +0800

    Add constants

---
+++ x/bulletin/types/constants.go
@@ -0,0 +1,14 @@
+package types
+
+const (
+	// NamespaceResource
+	NamespaceResource = "namespace"
+	// NamespaceIdPrefix
+	NamespaceIdPrefix = "bulletin/"
+	// CreatePostPermission
+	CreatePostPermission = "create_post"
+	// ManageCollaboratorsPermission
+	ManageCollaboratorsPermission = "_can_manage_collaborator"
+	// CollaboratorRelation
+	CollaboratorRelation = "collaborator"
+)
