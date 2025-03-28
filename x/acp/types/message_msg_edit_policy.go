5096209503d4e18d1f36ac9543e36b5a2fddee2a
Bruno Gomes <gomes.bruno.ac@gmail.com>
2025-03-28 15:12:54 -0300

    fix: MsgEditPolicy constructor

--- x/acp/types/message_msg_edit_policy.go
+++ x/acp/types/message_msg_edit_policy.go
@@ -2,13 +2,16 @@ package types
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	coretypes "github.com/sourcenetwork/acp_core/pkg/types"
 )
 
 var _ sdk.Msg = &MsgEditPolicy{}
 
-func NewMsgMsgEditPolicy(creator string, policyId string) *MsgEditPolicy {
+func NewMsgMsgEditPolicy(creator string, policyId string, policy string, marshalType coretypes.PolicyMarshalingType) *MsgEditPolicy {
 	return &MsgEditPolicy{
-		Creator:  creator,
-		PolicyId: policyId,
+		Creator:     creator,
+		PolicyId:    policyId,
+		Policy:      policy,
+		MarshalType: marshalType,
 	}
 }
