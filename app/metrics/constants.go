91fd4c2b6e6883c7ec9e344ab4c8ae318a49103e
Ivan <ivanverch@gmail.com>
2025-03-31 00:12:15 +0800

    Add constants

---
+++ app/metrics/constants.go
@@ -0,0 +1,19 @@
+package metrics
+
+const (
+	Count       = "count"
+	Latency     = "latency"
+	Method      = "method"
+	Transaction = "transaction"
+
+	PrepareProposal = "prepare_proposal"
+	ProcessProposal = "process_proposal"
+
+	CancelUnlocking   = "cancel_unlocking"
+	CompleteUnlocking = "complete_unlocking"
+	Lock              = "lock"
+	Redelegate        = "redelegate"
+	Unlock            = "unlock"
+
+	TotalLocked = "total_locked"
+)
