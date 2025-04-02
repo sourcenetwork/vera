ae0a3632bfdde0c7788237ac123708f9f3a45b61
Ivan <ivanverch@gmail.com>
2025-04-02 21:03:43 +0800

    Add setupKeeper helper

---
+++ x/bulletin/keeper/keeper_common_test.go
@@ -0,0 +1,81 @@
+package keeper
+
+import (
+	"testing"
+
+	"cosmossdk.io/log"
+	"cosmossdk.io/store"
+	"cosmossdk.io/store/metrics"
+	storetypes "cosmossdk.io/store/types"
+	cmtproto "github.com/cometbft/cometbft/proto/tendermint/types"
+	dbm "github.com/cosmos/cosmos-db"
+	"github.com/cosmos/cosmos-sdk/codec"
+	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
+	"github.com/cosmos/cosmos-sdk/runtime"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	authcodec "github.com/cosmos/cosmos-sdk/x/auth/codec"
+	authkeeper "github.com/cosmos/cosmos-sdk/x/auth/keeper"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
+	"github.com/stretchr/testify/require"
+
+	acpkeeper "github.com/sourcenetwork/sourcehub/x/acp/keeper"
+	"github.com/sourcenetwork/sourcehub/x/bulletin/types"
+)
+
+func setupKeeper(t testing.TB) (Keeper, sdk.Context) {
+	storeKey := storetypes.NewKVStoreKey(types.StoreKey)
+	authStoreKey := storetypes.NewKVStoreKey(authtypes.StoreKey)
+
+	db := dbm.NewMemDB()
+	stateStore := store.NewCommitMultiStore(db, log.NewNopLogger(), metrics.NewNoOpMetrics())
+	stateStore.MountStoreWithDB(storeKey, storetypes.StoreTypeDB, db)
+	stateStore.MountStoreWithDB(authStoreKey, storetypes.StoreTypeDB, db)
+	require.NoError(t, stateStore.LoadLatestVersion())
+
+	registry := codectypes.NewInterfaceRegistry()
+	authtypes.RegisterInterfaces(registry)
+
+	cdc := codec.NewProtoCodec(registry)
+	authority := authtypes.NewModuleAddress(govtypes.ModuleName)
+	bech32Prefix := "source"
+	addressCodec := authcodec.NewBech32Codec(bech32Prefix)
+
+	maccPerms := map[string][]string{
+		authtypes.FeeCollectorName: nil,
+		types.ModuleName:           nil,
+	}
+
+	accountKeeper := authkeeper.NewAccountKeeper(
+		cdc,
+		runtime.NewKVStoreService(authStoreKey),
+		authtypes.ProtoBaseAccount,
+		maccPerms,
+		addressCodec,
+		bech32Prefix,
+		authority.String(),
+	)
+
+	acpKeeper := acpkeeper.NewKeeper(
+		cdc,
+		runtime.NewKVStoreService(storeKey),
+		log.NewNopLogger(),
+		authority.String(),
+		accountKeeper,
+	)
+
+	k := NewKeeper(
+		cdc,
+		runtime.NewKVStoreService(storeKey),
+		log.NewNopLogger(),
+		authority.String(),
+		accountKeeper,
+		acpKeeper,
+	)
+
+	ctx := sdk.NewContext(stateStore, cmtproto.Header{}, false, log.NewNopLogger())
+
+	k.SetParams(ctx, types.DefaultParams())
+
+	return k, ctx
+}
