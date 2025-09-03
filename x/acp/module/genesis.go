5a9f2d121327441fc74f41c6d0aa6d649aaf5069
Ivan <ivanverch@gmail.com>
2025-09-03 14:35:39 +0400

    Update acp genesis

--- x/acp/module/genesis.go
+++ x/acp/module/genesis.go
@@ -9,8 +9,20 @@ import (
 
 // InitGenesis initializes the module's state from a provided genesis state.
 func InitGenesis(ctx sdk.Context, k *keeper.Keeper, genState types.GenesisState) {
-	// this line is used by starport scaffolding # genesis/module/init
 	k.SetParams(ctx, genState.Params)
+
+	// Initialize ica connections
+	for _, icaConnection := range genState.IcaConnections {
+		if err := k.SetICAConnection(
+			ctx,
+			icaConnection.IcaAddress,
+			icaConnection.ControllerAddress,
+			icaConnection.ControllerChainId,
+			icaConnection.ConnectionId,
+		); err != nil {
+			panic(err)
+		}
+	}
 }
 
 // ExportGenesis returns the module's exported genesis.
@@ -18,7 +30,8 @@ func ExportGenesis(ctx sdk.Context, k *keeper.Keeper) *types.GenesisState {
 	genesis := types.DefaultGenesis()
 	genesis.Params = k.GetParams(ctx)
 
-	// this line is used by starport scaffolding # genesis/module/export
+	// Export ica connections
+	genesis.IcaConnections = k.GetAllICAConnections(ctx)
 
 	return genesis
 }
