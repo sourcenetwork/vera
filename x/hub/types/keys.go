438103a6108a0ed1038bf26037ad6d8212e9d2f0
Ivan <ivanverch@gmail.com>
2025-10-21 20:37:12 +0400

    Add key prefix helpers

--- x/hub/types/keys.go
+++ x/hub/types/keys.go
@@ -1,5 +1,10 @@
 package types
 
+import (
+	"crypto/sha256"
+	"encoding/hex"
+)
+
 const (
 	// ModuleName defines the module name
 	ModuleName = "hub"
@@ -34,3 +39,40 @@ var (
 	// Set during genesis initialization and never changed.
 	IgnoreBearerAuthKey = []byte("app_config/ignore_bearer_auth")
 )
+
+// HashJWSToken returns the SHA256 hash of a JWS token string.
+func HashJWSToken(jwsToken string) string {
+	hash := sha256.Sum256([]byte(jwsToken))
+	return hex.EncodeToString(hash[:])
+}
+
+// JWSTokenKey returns the store key for a JWS token by its hash.
+func JWSTokenKey(tokenHash string) []byte {
+	return append([]byte(JWSTokenKeyPrefix), []byte(tokenHash)...)
+}
+
+// JWSTokenByDIDKey returns the store key for a JWS token indexed by DID and hash.
+func JWSTokenByDIDKey(did, tokenHash string) []byte {
+	didKey := append([]byte(JWSTokenByDIDKeyPrefix), []byte(did)...)
+	didKey = append(didKey, '/')
+	return append(didKey, []byte(tokenHash)...)
+}
+
+// JWSTokenByAccountKey returns the store key for a JWS token indexed by account and hash.
+func JWSTokenByAccountKey(account, tokenHash string) []byte {
+	accountKey := append([]byte(JWSTokenByAccountKeyPrefix), []byte(account)...)
+	accountKey = append(accountKey, '/')
+	return append(accountKey, []byte(tokenHash)...)
+}
+
+// JWSTokenDIDPrefix returns the prefix for all tokens belonging to a DID.
+func JWSTokenDIDPrefix(did string) []byte {
+	prefix := append([]byte(JWSTokenByDIDKeyPrefix), []byte(did)...)
+	return append(prefix, '/')
+}
+
+// JWSTokenAccountPrefix returns the prefix for all tokens belonging to an account.
+func JWSTokenAccountPrefix(account string) []byte {
+	prefix := append([]byte(JWSTokenByAccountKeyPrefix), []byte(account)...)
+	return append(prefix, '/')
+}
