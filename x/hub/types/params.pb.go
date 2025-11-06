ccfbb4c39ab9ea536285c53a477a21cf37d57ff2
Bruno Gomes <gomes.bruno.ac@gmail.com>
2025-11-06 10:13:24 -0300

    hub genesis params

--- x/hub/types/params.pb.go
+++ x/hub/types/params.pb.go
@@ -26,7 +26,6 @@ const _ = proto.GoGoProtoPackageIsVersion3 // please upgrade the proto package
 
 // Params defines the parameters for the module.
 type Params struct {
-	GenesisParams *GenesisParams `protobuf:"bytes,1,opt,name=genesis_params,json=genesisParams,proto3" json:"genesis_params,omitempty"`
 }
 
 func (m *Params) Reset()         { *m = Params{} }
@@ -62,96 +61,25 @@ func (m *Params) XXX_DiscardUnknown() {
 
 var xxx_messageInfo_Params proto.InternalMessageInfo
 
-func (m *Params) GetGenesisParams() *GenesisParams {
-	if m != nil {
-		return m.GenesisParams
-	}
-	return nil
-}
-
-type GenesisParams struct {
-	// allow_zero_fee_txs if true configures the antee handler
-	// to not reject txs with 0 fees
-	AllowZeroFeeTxs bool `protobuf:"varint,1,opt,name=allow_zero_fee_txs,json=allowZeroFeeTxs,proto3" json:"allow_zero_fee_txs,omitempty"`
-	// ignore_bearer_auth if true ignores the `authorized_account`
-	// field in the Bearer JWS from Extension Options
-	IgnoreBearerAuth bool `protobuf:"varint,2,opt,name=ignore_bearer_auth,json=ignoreBearerAuth,proto3" json:"ignore_bearer_auth,omitempty"`
-}
-
-func (m *GenesisParams) Reset()         { *m = GenesisParams{} }
-func (m *GenesisParams) String() string { return proto.CompactTextString(m) }
-func (*GenesisParams) ProtoMessage()    {}
-func (*GenesisParams) Descriptor() ([]byte, []int) {
-	return fileDescriptor_23e5f04c1d8b06a7, []int{1}
-}
-func (m *GenesisParams) XXX_Unmarshal(b []byte) error {
-	return m.Unmarshal(b)
-}
-func (m *GenesisParams) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
-	if deterministic {
-		return xxx_messageInfo_GenesisParams.Marshal(b, m, deterministic)
-	} else {
-		b = b[:cap(b)]
-		n, err := m.MarshalToSizedBuffer(b)
-		if err != nil {
-			return nil, err
-		}
-		return b[:n], nil
-	}
-}
-func (m *GenesisParams) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_GenesisParams.Merge(m, src)
-}
-func (m *GenesisParams) XXX_Size() int {
-	return m.Size()
-}
-func (m *GenesisParams) XXX_DiscardUnknown() {
-	xxx_messageInfo_GenesisParams.DiscardUnknown(m)
-}
-
-var xxx_messageInfo_GenesisParams proto.InternalMessageInfo
-
-func (m *GenesisParams) GetAllowZeroFeeTxs() bool {
-	if m != nil {
-		return m.AllowZeroFeeTxs
-	}
-	return false
-}
-
-func (m *GenesisParams) GetIgnoreBearerAuth() bool {
-	if m != nil {
-		return m.IgnoreBearerAuth
-	}
-	return false
-}
-
 func init() {
 	proto.RegisterType((*Params)(nil), "sourcehub.hub.Params")
-	proto.RegisterType((*GenesisParams)(nil), "sourcehub.hub.GenesisParams")
 }
 
 func init() { proto.RegisterFile("sourcehub/hub/params.proto", fileDescriptor_23e5f04c1d8b06a7) }
 
 var fileDescriptor_23e5f04c1d8b06a7 = []byte{
-	// 286 bytes of a gzipped FileDescriptorProto
+	// 168 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x92, 0x2a, 0xce, 0x2f, 0x2d,
 	0x4a, 0x4e, 0xcd, 0x28, 0x4d, 0xd2, 0x07, 0xe1, 0x82, 0xc4, 0xa2, 0xc4, 0xdc, 0x62, 0xbd, 0x82,
 	0xa2, 0xfc, 0x92, 0x7c, 0x21, 0x5e, 0xb8, 0x9c, 0x5e, 0x46, 0x69, 0x92, 0x94, 0x60, 0x62, 0x6e,
 	0x66, 0x5e, 0xbe, 0x3e, 0x98, 0x84, 0xa8, 0x90, 0x12, 0x49, 0xcf, 0x4f, 0xcf, 0x07, 0x33, 0xf5,
-	0x41, 0x2c, 0x88, 0xa8, 0x52, 0x1e, 0x17, 0x5b, 0x00, 0xd8, 0x1c, 0x21, 0x67, 0x2e, 0xbe, 0xf4,
-	0xd4, 0xbc, 0xd4, 0xe2, 0xcc, 0xe2, 0x78, 0x88, 0xc9, 0x12, 0x8c, 0x0a, 0x8c, 0x1a, 0xdc, 0x46,
-	0x32, 0x7a, 0x28, 0x46, 0xeb, 0xb9, 0x43, 0x14, 0x41, 0x74, 0x05, 0xf1, 0xa6, 0x23, 0x73, 0xad,
-	0xe4, 0x5f, 0x2c, 0x90, 0x67, 0xec, 0x7a, 0xbe, 0x41, 0x4b, 0x0c, 0xe1, 0xd6, 0x0a, 0xb0, 0x6b,
-	0x21, 0x0a, 0x94, 0xb2, 0xb8, 0x78, 0x51, 0x0c, 0x10, 0xd2, 0xe6, 0x12, 0x4a, 0xcc, 0xc9, 0xc9,
-	0x2f, 0x8f, 0xaf, 0x4a, 0x2d, 0xca, 0x8f, 0x4f, 0x4b, 0x4d, 0x8d, 0x2f, 0xa9, 0x80, 0x58, 0xcd,
-	0x11, 0xc4, 0x0f, 0x96, 0x89, 0x4a, 0x2d, 0xca, 0x77, 0x4b, 0x4d, 0x0d, 0xa9, 0x28, 0x16, 0xd2,
-	0xe1, 0x12, 0xca, 0x4c, 0xcf, 0xcb, 0x2f, 0x4a, 0x8d, 0x4f, 0x4a, 0x4d, 0x2c, 0x4a, 0x2d, 0x8a,
-	0x4f, 0x2c, 0x2d, 0xc9, 0x90, 0x60, 0x02, 0x2b, 0x16, 0x80, 0xc8, 0x38, 0x81, 0x25, 0x1c, 0x4b,
-	0x4b, 0x32, 0x9c, 0x3c, 0x4e, 0x3c, 0x92, 0x63, 0xbc, 0xf0, 0x48, 0x8e, 0xf1, 0xc1, 0x23, 0x39,
-	0xc6, 0x09, 0x8f, 0xe5, 0x18, 0x2e, 0x3c, 0x96, 0x63, 0xb8, 0xf1, 0x58, 0x8e, 0x21, 0x4a, 0x2f,
-	0x3d, 0xb3, 0x04, 0xe4, 0x97, 0xe4, 0xfc, 0x5c, 0x7d, 0x88, 0x43, 0xf3, 0x52, 0x4b, 0xca, 0xf3,
-	0x8b, 0xb2, 0xf5, 0xd1, 0x9d, 0x5d, 0x52, 0x59, 0x90, 0x5a, 0x9c, 0xc4, 0x06, 0x0e, 0x2c, 0x63,
-	0x40, 0x00, 0x00, 0x00, 0xff, 0xff, 0x23, 0x53, 0x2f, 0x0f, 0x82, 0x01, 0x00, 0x00,
+	0x41, 0x2c, 0x88, 0xa8, 0x92, 0x26, 0x17, 0x5b, 0x00, 0xd8, 0x1c, 0x2b, 0xf9, 0x17, 0x0b, 0xe4,
+	0x19, 0xbb, 0x9e, 0x6f, 0xd0, 0x12, 0x43, 0x58, 0x53, 0x01, 0xb6, 0x08, 0xa2, 0xc0, 0xc9, 0xe3,
+	0xc4, 0x23, 0x39, 0xc6, 0x0b, 0x8f, 0xe4, 0x18, 0x1f, 0x3c, 0x92, 0x63, 0x9c, 0xf0, 0x58, 0x8e,
+	0xe1, 0xc2, 0x63, 0x39, 0x86, 0x1b, 0x8f, 0xe5, 0x18, 0xa2, 0xf4, 0xd2, 0x33, 0x4b, 0x40, 0x36,
+	0x27, 0xe7, 0xe7, 0xea, 0x43, 0x34, 0xe7, 0xa5, 0x96, 0x94, 0xe7, 0x17, 0x65, 0xeb, 0xa3, 0x1b,
+	0x55, 0x52, 0x59, 0x90, 0x5a, 0x9c, 0xc4, 0x06, 0xb6, 0xdb, 0x18, 0x10, 0x00, 0x00, 0xff, 0xff,
+	0xb2, 0x62, 0xf3, 0xf7, 0xd1, 0x00, 0x00, 0x00,
 }
 
 func (this *Params) Equal(that interface{}) bool {
@@ -171,9 +99,6 @@ func (this *Params) Equal(that interface{}) bool {
 	if that1 == nil {
 		return this == nil
 	} else if this == nil {
-		return false
-	}
-	if !this.GenesisParams.Equal(that1.GenesisParams) {
 		return false
 	}
 	return true
@@ -198,61 +123,6 @@ func (m *Params) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 	_ = i
 	var l int
 	_ = l
-	if m.GenesisParams != nil {
-		{
-			size, err := m.GenesisParams.MarshalToSizedBuffer(dAtA[:i])
-			if err != nil {
-				return 0, err
-			}
-			i -= size
-			i = encodeVarintParams(dAtA, i, uint64(size))
-		}
-		i--
-		dAtA[i] = 0xa
-	}
-	return len(dAtA) - i, nil
-}
-
-func (m *GenesisParams) Marshal() (dAtA []byte, err error) {
-	size := m.Size()
-	dAtA = make([]byte, size)
-	n, err := m.MarshalToSizedBuffer(dAtA[:size])
-	if err != nil {
-		return nil, err
-	}
-	return dAtA[:n], nil
-}
-
-func (m *GenesisParams) MarshalTo(dAtA []byte) (int, error) {
-	size := m.Size()
-	return m.MarshalToSizedBuffer(dAtA[:size])
-}
-
-func (m *GenesisParams) MarshalToSizedBuffer(dAtA []byte) (int, error) {
-	i := len(dAtA)
-	_ = i
-	var l int
-	_ = l
-	if m.IgnoreBearerAuth {
-		i--
-		if m.IgnoreBearerAuth {
-			dAtA[i] = 1
-		} else {
-			dAtA[i] = 0
-		}
-		i--
-		dAtA[i] = 0x10
-	}
-	if m.AllowZeroFeeTxs {
-		i--
-		if m.AllowZeroFeeTxs {
-			dAtA[i] = 1
-		} else {
-			dAtA[i] = 0
-		}
-		i--
-		dAtA[i] = 0x8
-	}
 	return len(dAtA) - i, nil
 }
 
@@ -273,25 +143,6 @@ func (m *Params) Size() (n int) {
 	}
 	var l int
 	_ = l
-	if m.GenesisParams != nil {
-		l = m.GenesisParams.Size()
-		n += 1 + l + sovParams(uint64(l))
-	}
-	return n
-}
-
-func (m *GenesisParams) Size() (n int) {
-	if m == nil {
-		return 0
-	}
-	var l int
-	_ = l
-	if m.AllowZeroFeeTxs {
-		n += 2
-	}
-	if m.IgnoreBearerAuth {
-		n += 2
-	}
 	return n
 }
 
@@ -330,132 +181,6 @@ func (m *Params) Unmarshal(dAtA []byte) error {
 			return fmt.Errorf("proto: Params: illegal tag %d (wire type %d)", fieldNum, wire)
 		}
 		switch fieldNum {
-		case 1:
-			if wireType != 2 {
-				return fmt.Errorf("proto: wrong wireType = %d for field GenesisParams", wireType)
-			}
-			var msglen int
-			for shift := uint(0); ; shift += 7 {
-				if shift >= 64 {
-					return ErrIntOverflowParams
-				}
-				if iNdEx >= l {
-					return io.ErrUnexpectedEOF
-				}
-				b := dAtA[iNdEx]
-				iNdEx++
-				msglen |= int(b&0x7F) << shift
-				if b < 0x80 {
-					break
-				}
-			}
-			if msglen < 0 {
-				return ErrInvalidLengthParams
-			}
-			postIndex := iNdEx + msglen
-			if postIndex < 0 {
-				return ErrInvalidLengthParams
-			}
-			if postIndex > l {
-				return io.ErrUnexpectedEOF
-			}
-			if m.GenesisParams == nil {
-				m.GenesisParams = &GenesisParams{}
-			}
-			if err := m.GenesisParams.Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
-				return err
-			}
-			iNdEx = postIndex
-		default:
-			iNdEx = preIndex
-			skippy, err := skipParams(dAtA[iNdEx:])
-			if err != nil {
-				return err
-			}
-			if (skippy < 0) || (iNdEx+skippy) < 0 {
-				return ErrInvalidLengthParams
-			}
-			if (iNdEx + skippy) > l {
-				return io.ErrUnexpectedEOF
-			}
-			iNdEx += skippy
-		}
-	}
-
-	if iNdEx > l {
-		return io.ErrUnexpectedEOF
-	}
-	return nil
-}
-func (m *GenesisParams) Unmarshal(dAtA []byte) error {
-	l := len(dAtA)
-	iNdEx := 0
-	for iNdEx < l {
-		preIndex := iNdEx
-		var wire uint64
-		for shift := uint(0); ; shift += 7 {
-			if shift >= 64 {
-				return ErrIntOverflowParams
-			}
-			if iNdEx >= l {
-				return io.ErrUnexpectedEOF
-			}
-			b := dAtA[iNdEx]
-			iNdEx++
-			wire |= uint64(b&0x7F) << shift
-			if b < 0x80 {
-				break
-			}
-		}
-		fieldNum := int32(wire >> 3)
-		wireType := int(wire & 0x7)
-		if wireType == 4 {
-			return fmt.Errorf("proto: GenesisParams: wiretype end group for non-group")
-		}
-		if fieldNum <= 0 {
-			return fmt.Errorf("proto: GenesisParams: illegal tag %d (wire type %d)", fieldNum, wire)
-		}
-		switch fieldNum {
-		case 1:
-			if wireType != 0 {
-				return fmt.Errorf("proto: wrong wireType = %d for field AllowZeroFeeTxs", wireType)
-			}
-			var v int
-			for shift := uint(0); ; shift += 7 {
-				if shift >= 64 {
-					return ErrIntOverflowParams
-				}
-				if iNdEx >= l {
-					return io.ErrUnexpectedEOF
-				}
-				b := dAtA[iNdEx]
-				iNdEx++
-				v |= int(b&0x7F) << shift
-				if b < 0x80 {
-					break
-				}
-			}
-			m.AllowZeroFeeTxs = bool(v != 0)
-		case 2:
-			if wireType != 0 {
-				return fmt.Errorf("proto: wrong wireType = %d for field IgnoreBearerAuth", wireType)
-			}
-			var v int
-			for shift := uint(0); ; shift += 7 {
-				if shift >= 64 {
-					return ErrIntOverflowParams
-				}
-				if iNdEx >= l {
-					return io.ErrUnexpectedEOF
-				}
-				b := dAtA[iNdEx]
-				iNdEx++
-				v |= int(b&0x7F) << shift
-				if b < 0x80 {
-					break
-				}
-			}
-			m.IgnoreBearerAuth = bool(v != 0)
 		default:
 			iNdEx = preIndex
 			skippy, err := skipParams(dAtA[iNdEx:])
