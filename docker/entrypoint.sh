ec436fb27cf447ce4fac85e1d108ae3188f2df0b
Bruno Gomes <gomes.bruno.ac@gmail.com>
2026-01-15 16:51:56 -0300

    fix entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,7 +6,7 @@ DEFAULT_CHAIN_ID="sourcehub"
 DEFAULT_MONIKER="node"
 DEV_FACUET_MNEMONIC="comic very pond victory suit tube ginger antique life then core warm loyal deliver iron fashion erupt husband weekend monster sunny artist empty uphold"
 
-if [ ! -d /sourcehub/.initialized ]; then
+if [ ! -f /sourcehub/config/genesis.json ]; then
     echo "Initializing SourceHub"
 
     if [ -z "$CHAIN_ID" ]; then 
