6a4b764d18ba54e5e3741faf36cbfe3603c1943b
iverc <ivanverch@gmail.com>
2026-04-22 05:49:20 +0700

    Add update post msg type

---
+++ x/bulletin/types/message_update_post.go
@@ -0,0 +1,34 @@
+package types
+
+import (
+	errorsmod "cosmossdk.io/errors"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+)
+
+var _ sdk.Msg = &MsgUpdatePost{}
+
+func NewMsgUpdatePost(creator, namespace, postId string, payload []byte) *MsgUpdatePost {
+	return &MsgUpdatePost{
+		Creator:   creator,
+		Namespace: namespace,
+		PostId:    postId,
+		Payload:   payload,
+	}
+}
+
+func (msg *MsgUpdatePost) ValidateBasic() error {
+	if _, err := sdk.AccAddressFromBech32(msg.Creator); err != nil {
+		return errorsmod.Wrapf(sdkerrors.ErrInvalidAddress, "invalid creator address (%s)", err)
+	}
+	if msg.Namespace == "" {
+		return ErrInvalidNamespaceId
+	}
+	if msg.PostId == "" {
+		return ErrPostNotFound
+	}
+	if len(msg.Payload) == 0 {
+		return ErrInvalidPostPayload
+	}
+	return nil
+}
